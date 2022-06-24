a92c3b6b7f5a86c97e9ab479503ae99b0f998e40
Venky80493 <51815676+Venky80493@users.noreply.github.com>
2022-06-24 09:34:04 -0700

    Time: 206 ms (87.88%), Space: 15.4 MB (93.05%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.py
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.py
@@ -5,5 +5,5 @@ class Solution:
         while i>=0:
             if nums[i]+nums[i+1]>nums[i+2]:
                 return nums[i]+nums[i+1]+nums[i+2]
-            i=i-1
+            i-=1
         return 0
