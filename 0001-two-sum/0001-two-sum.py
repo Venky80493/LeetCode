488e7a63727795402555a30b940df86af3871c10
Venky80493 <51815676+Venky80493@users.noreply.github.com>
2024-02-04 14:21:07 -0800

    Time: 51 ms (95.50%), Space: 17.8 MB (58.00%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,8 +2,9 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         traversed_nums={}
         for i in range(len(nums)):
-            if target-nums[i] in traversed_nums.keys():
-                return [traversed_nums[target-nums[i]],i]
+            diff=target-nums[i]
+            if diff in traversed_nums:
+                return [traversed_nums[diff],i]
             else:
                 traversed_nums[nums[i]]=i
         return False
